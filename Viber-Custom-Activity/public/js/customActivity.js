b96df9ce2f77036c149fb8e629505826dbd7196b
impetiue <83606663+impetiue@users.noreply.github.com>
2021-12-10 14:24:20 +0530

    Update customActivity.js

--- Viber-Custom-Activity/public/js/customActivity.js
+++ Viber-Custom-Activity/public/js/customActivity.js
@@ -20,7 +20,7 @@ define([
     connection.on('requestedEndpoints', onGetEndpoints);
     
     connection.on('clickedNext', onClickedNext);
-    connection.on('clickedBack', onClickedBack);
+    //connection.on('clickedBack', onClickedBack);
     connection.on('gotoStep', onGotoStep);
    // connection.on('clickedNext', save);
     //connection.on('clickedBack', onClickedBack);
